798982781f088343fa5d0af673998cb4e5bd84f8
442413768 <442413768@qq.com>
2022-01-11 22:07:32 +0800

    nav

--- src/common/HeadNav.js
+++ src/common/HeadNav.js
@@ -1,18 +1,19 @@
 import React, {Component} from 'react'
 import {Menu} from 'antd'
-import './HeadNav.less'
+
 import './HeadNav.css'
-import {SmileOutlined} from '@ant-design/icons';
+import {GithubOutlined} from '@ant-design/icons';
 export default class HeadNav extends Component{
     state = {
         current: 'home'
     }
     render(){
         return (
+            <div>
             <div id="HeadNav">
                 <div className="nav-wrap">
                     <div className="nav-logo-wrap">
-                    <SmileOutlined type="global" className="nav-logo"></SmileOutlined>
+                    <GithubOutlined  type="global" className="nav-logo"/>
                     </div>
                     <div className="nav-list-wrap">
                         <Menu mode="horizontal">
@@ -31,7 +32,26 @@ export default class HeadNav extends Component{
                         </Menu>
                     </div>
                 </div>
-            </div>    
+            </div>
+            {/*<div id="ButtonContent">
+            <div className="nav-list-wrap">
+                        <Menu mode="horizontal">
+                                <Menu.Item key="home">
+                                    Contact
+                                </Menu.Item>
+                                <Menu.Item key="aboutme">
+                                    联系我
+                                </Menu.Item>
+                                <Menu.Item key="articles">
+                                    所有文章
+                                </Menu.Item>
+                                <Menu.Item key="ArticleClass">
+                                    文章分类
+                                </Menu.Item>
+                        </Menu>
+                    </div>
+            </div>*/}
+                </div>
         )
     }
 }
