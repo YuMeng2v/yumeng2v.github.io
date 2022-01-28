6e77718d3a026abebaec4a491940232a82353ca7
442413768 <442413768@qq.com>
2022-01-28 11:22:23 +0800

    made it

--- src/components/OneMd.js
+++ src/components/OneMd.js
@@ -2,7 +2,7 @@ import React,{Component} from 'react'
 import ReactMarkdown from 'react-markdown'
 import './file.css'
 import HeadNav from '../common/HeadNav'
-
+//import * as fs from 'fs';//fs in node not in browser
 //如何让子组件知道自己是哪一篇
 import {useLocation} from 'react-router-dom';
 //将所有markdown都引入
@@ -11,30 +11,38 @@ function importAll(r){
 }
 //require.context： 代表达式的require语句
 //
+
 const markdownContext = require.context('../articles/markdown',false,/\.md/);
 const markdownContext1 = require.context('../articles/images',false,/\.png/);
 const markdownFiles = markdownContext.keys().map((filename)=>markdownContext(filename))
 console.log('Onemd',markdownFiles[0]);
+
+let contents = [];
 class OneMd extends Component {
     constructor(props){
         super(props)  
         //初始化状态，每次刷新也会调用这个函数
-        this.state = {src:'',posts:['# loading']};
+        this.state = {src:0,posts:''};
     }
-    componentDidMount(){
+    componentWillMount(){
         if(this.props.match!=undefined){
-            this.setState({src:this.props.match.url.split('/')[2]})
-            //无法立即输出更新后的值
+            this.setState({src:Number(this.props.match.url.split('/')[2])})
         }
     }
+    componentDidMount(){
+        fetch(markdownFiles[this.state.src]).then(res=>res.text()).then(text=>this.setState({posts:text}));
+    }
     componentDidUpdate(){
+        //const content = fileReader.readAsText(markdownFiles[this.state.src])
+        //this.setState({posts:contents[this.state.src]})
+        //console.log(content);
         console.log('OneMd:',this.state);
     }
     render(){
         return  (
             <div>
                     <HeadNav/>
-                    <ReactMarkdown source={markdownFiles[this.state.src]}/>
+                    <ReactMarkdown source={this.state.posts}/>
             </div>
             )
         }
